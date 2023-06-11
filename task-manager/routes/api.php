5267df4c1d376b738004a621371084fe8d38d8a4
karasicevic <ak20190190@student.fon.bg.ac.rs>
2023-06-11 17:19:46 +0200

    dodate rute

--- task-manager/routes/api.php
+++ task-manager/routes/api.php
@@ -25,3 +25,24 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::resource('users', UserController::class);
+Route::resource('projects', ProjectController::class);
+Route::resource('tasks', TaskController::class);
+
+Route::get('/users/{id}/tasks', [UserTaskController::class, 'index'])->name('users.tasks.index');
+Route::get('/project/{id}/tasks', [ProjectTaskController::class, 'index'])->name('projects.tasks.index');
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/profile', function(Request $request) {
+        return auth()->user();
+    });
+    Route::resource('posts', PostController::class)->only(['update','store','destroy']);
+
+    // API route for logout user
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
